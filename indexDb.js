d8a97033952a800ed67ef9c919da70a79e73589c
Wayne <seriawei@gmail.com>
2016-01-05 17:20:23 +0800

    .

--- indexDb.js
+++ indexDb.js
@@ -1,27 +1,52 @@
+var DbContext = {
+	DataBase: null,
+	getOpenSetting: function (onSuccess) {
+		DbContext.DataBase
+			.transaction(["forToken"], "readwrite")
+			.objectStore("forToken")
+			.get("1").onsuccess = function (e) {
+				if (e.target.result) {
+					if (onSuccess) {
+						onSuccess.call(this, e.target.result);
+					}
+				}
+				else {
+					var setting = {
+						id: "1",
+						AccessKey: "",
+						SecretKey: "",
+						OpenKey: ""
+					};
+					DbContext.DataBase
+						.transaction(["forToken"], "readwrite")
+						.objectStore("forToken")
+						.add(setting).onsuccess = function (e) {
+							if (onSuccess) {
+								onSuccess.call(this, setting);
+							}
+						}
+				}
+			};
+	},
+	updateOpenSetting: function (setting, onSuccess) {
+		DbContext.DataBase
+			.transaction(["forToken"], "readwrite")
+			.transaction.objectStore("forToken")
+			.put(setting).onsuccess = function (e) {
+				if (onSuccess) {
+					onSuccess.call(this, e.target.result);
+				}
+			};
+	}
+}
+
 var dbRequest = window.indexedDB.open("keys", 1);
 
 dbRequest.onsuccess = function (e) {
-	var dataBase = e.target.result;
-
-	var transaction = dataBase.transaction(["forToken"], "readwrite");
-	var store = transaction.objectStore("forToken");;
-	// store.add({
-	// 	id: "1",
-	// 	AccessKey: "8785e5649fcb34a820f4166de62887851a303404",
-	// 	SecretKey: "f6da983de97064ae36a467aa6bf0dce071a0cfd9",
-	// 	OpenKey: "cGyemsdmaGmdzMaSlGTFmmJmzGZjmZiYnGloa2ppb21plpmVlWViacOblGiSYpU="
-	// });
-			
-	store.get("1").onsuccess = function (e) {
-		debugger
-		var token = e.target.result;
-	};
-	//store.put();
+	DbContext.DataBase = e.target.result;
 }
 dbRequest.onupgradeneeded = function (e) {
-	debugger
 	var thisDB = e.target.result;
-
 	if (!thisDB.objectStoreNames.contains("forToken")) {
 		thisDB.createObjectStore("forToken", {  
 			// primary key  
